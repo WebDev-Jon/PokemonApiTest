0d06bfdd96e75831f9a109eecaecdfd859d09df0
Jonathan <Jonathan@DESKTOP-AOQND0D>
2023-04-04 12:18:27 +0200

    Passage en Pokéapi fr

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ let pokeName = document.getElementById("name");
 const changePokemon = async() => {
     let randomNumber = Math.ceil(Math.random() * 1009 + 1);
 
-    let requestString = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
-
+    let requestString = `https://api-pokemon-fr.vercel.app/api/v1/pokemon/${randomNumber}`;
+    
     let data = await fetch(requestString)
     console.log(data);
 
     let response = await data.json();
     console.log(response);
 
-    image.src = response.sprites.front_default;
-    pokeNumber.textContent = `#${response.id}`;
-    pokeName.textContent = response.name;
+    image.src = response.sprites.regular;
+    pokeNumber.textContent = `#${response.pokedexId}`;
+    pokeName.textContent = response.name.fr;
 }
 
 changePokemon();
